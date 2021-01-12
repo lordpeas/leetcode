7799df636aad0cafac79dfda610f97b84f9f5e21
cuitianqi <cuitianqi@idstaff.com>
2021-01-12 21:20:28 +0800

    最长回文子串

---
+++ src/LongestPalindromicSubstring.java
@@ -0,0 +1,32 @@
+
+//最长回文子串
+public class LongestPalindromicSubstring {
+
+
+    public String longestPalindrome(String s) {
+        String res = "";
+        for(int i = 0;i<s.length();i++){
+            String s1 =  palindrome(s,i,i);
+            String s2 =  palindrome(s,i,i+1);
+            res = res.length()<s1.length()?s1:res;
+            res = res.length()<s2.length()?s2:res;
+        }
+        return res;
+    }
+
+
+    private String palindrome(String s,int l,int r){
+        while(l>=0&&r<s.length()&&s.charAt(l)==s.charAt(r)){
+            l--;
+            r++;
+        }
+        return s.substring(l+1,r);
+    }
+
+
+    public static void main(String[] args) {
+        LongestPalindromicSubstring instance = new LongestPalindromicSubstring();
+        instance.longestPalindrome("0123abcdcbawq");
+
+    }
+}
