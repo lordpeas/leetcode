d437f4ee1be9c9f8bb2d6e0c1bb496a1dfc04f07
stone <stone_tq@163.com>
2021-05-12 18:16:27 +0800

    快速选择

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -1,12 +1,23 @@
 package sort;
 
-import jdk.nashorn.internal.objects.NativeUint8Array;
 
 public class QuickSort {
 
     public int findKthLargest(int[] nums, int k) {
-        sort(nums);
-        return nums[nums.length-k];
+        int lo = 0, hi = nums.length - 1;
+        k = nums.length - k;
+        while (lo <= hi) {
+            int p = partition(nums, lo, hi);
+            if (p < k) {
+                lo = p + 1;
+            } else if (p > k) {
+                hi = p - 1;
+            }else {
+             return nums[p];
+            }
+        }
+
+        return -1;
     }
 
     public void sort(int[] nums) {
@@ -27,16 +38,16 @@ public class QuickSort {
         int i = lo, j = hi + 1;
         while (true) {
             while (nums[++i] < pivot) {
-                if(i==hi) break;
+                if (i == hi) break;
             }
 
-            while (nums[--j] > pivot){
-                if (j==lo) break;
+            while (nums[--j] > pivot) {
+                if (j == lo) break;
             }
-            if(i>=j) break;
-            swap(nums,i,j);
+            if (i >= j) break;
+            swap(nums, i, j);
         }
-        swap(nums,lo,j);
+        swap(nums, lo, j);
         return j;
     }
 
@@ -49,7 +60,7 @@ public class QuickSort {
 
     public static void main(String[] args) {
         QuickSort quickSort = new QuickSort();
-       int[] nums = new int[]{4,1,6,3,2,5};
+        int[] nums = new int[]{4, 1, 6, 3, 2, 5};
         int kthLargest = quickSort.findKthLargest(nums, 2);
         System.out.println(kthLargest);
 
