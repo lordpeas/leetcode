3808ce3eba580b2de80ffe3ccf53e0946a37f003
cuitianqi <cuitianqi@idstaff.com>
2021-06-08 16:25:16 +0800

    练习

---
+++ src/HashMapTest.java
@@ -0,0 +1,15 @@
+/**
+ * Description: leetcode
+ * Created by : Tianqi Cui
+ * Create Date: 2021/5/27 15:24
+ */
+public class HashMapTest {
+
+    public static void main(String[] args) {
+        int i = 10;
+
+        double res = i % Math.pow(2, 3);
+        int i1 = i & (int) (Math.pow(2, 3) - 1);
+
+    }
+}
---
+++ src/pc/Consumer.java
@@ -0,0 +1,9 @@
+package pc;
+
+/**
+ * Description: leetcode
+ * Created by : Tianqi Cui
+ * Create Date: 2021/6/3 17:53
+ */
+public class Consumer {
+}
